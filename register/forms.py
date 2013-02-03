a23381c640b8701c196ce336fcdbcfa55710b3a1
David <david.kingman@gmail.com>
2013-02-03 13:04:06 -0600

    More registration work

--- register/forms.py
+++ register/forms.py
@@ -2,19 +2,39 @@ from django import forms
 from django.contrib.auth.models import User
 from django.forms import ModelForm
 from register.models import Intern
+from crispy_forms.helper import FormHelper
 
 class RegistrationForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+    
     MAJORS = (
     ('1', 'Accountancy'),
     ('2', 'Business Administration'),
     ('3', 'Computer Science'),
     ('4', 'Finance'),
     ('5', 'Information Technology'),
+    ('6', 'Mathematics'),
     )
     
-    username    = forms.CharField(label=(u'User Name'))
+    STATES = (
+    ('1', 'Alabama'),
+    ('2', 'Alaska'),
+    ('3', 'Arizona'),
+    ('4', 'Arkansas'),
+    ('5', 'California'),
+    )
+
+    name        = forms.CharField(label=(u'Name'))
+    address     = forms.CharField(label=(u'Address'))
+    city        = forms.CharField(label=(u'City'))
+    state       = forms.ChoiceField(label=(u'State'), choices=STATES)
+    zip         = forms.CharField(label=(u'Zip'))
+    school      = forms.CharField(label=(u'School'))
+    graduation  = forms.DateField(label=(u'Graduation Date'))   
     major       = forms.ChoiceField(label=(u'Major'), choices=MAJORS)
-#    major       = forms.BooleanField(label=(u'Major'))
+    username    = forms.CharField(label=(u'User Name'))
     email       = forms.EmailField(label=(u'Email Address'))
     password    = forms.CharField(label=(u'Password'), widget=forms.PasswordInput(render_value=False))
     password1   = forms.CharField(label=(u'Verify Password'), widget=forms.PasswordInput(render_value=False))
