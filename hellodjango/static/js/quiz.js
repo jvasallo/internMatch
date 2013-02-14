da5118021725030e403db14a14630faa117d979e
Juan Carlos Escobar <escobar.jc7@gmail.com>
2013-02-13 22:54:35 -0600

    changed POST data format of quiz to match format specifications and made corrections to quiz template

--- hellodjango/static/js/quiz.js
+++ hellodjango/static/js/quiz.js
@@ -1,43 +1,42 @@
-// Allow sortable container items
+/**
+ * Allow sortable container items
+ */
 $(function() {
 	$('#sortable').sortable();
 	$('#sortable').disableSelection();
 });
 
 window.onload = function(){
-	// Send button action
-	var quizObjectResults; 				// Quiz Object Results
-	var quizArrayResults = new Array(); // Quiz Array Results
-	//var quizStringResults; 				// Quiz String Results
-	var quizData;						// Quiz Data to POST
-	
-	$('#send-quiz').on('click', function() { 
-  	  quizObjectResults = $('#sortable .quiz-category');
-		var numberOfItems = quizObjectResults.length;
-		quizStringResults = new String();
+	/**
+	 * Submit button click action
+	 */
+	$('#submit-quiz').on('click', function() { 
+		var quizObjectResults = $('#sortable .quiz-category');	// sortable quiz object
+		var numberOfItems = quizObjectResults.length;			// total quiz items
+		var quizData = "[%USER_ID_HERE%] ";					// initialize new string to put quiz results
+		
+		// loop through quiz object to retrieve id order and store as string
 		for (var i=0; i<numberOfItems; i++) {
-			quizArrayResults[i] = quizObjectResults[i].id
-			// if (i < numberOfItems) {
-				// quizStringResults = quizStringResults + quizObjectResults[i].id + " "
-			// }
-			// else {
-				// quizStringResults = quizStringResults + quizObjectResults[i].id
-			// }
+			// only add space between id's if not last id in list
+			if (i < numberOfItems-1) {
+				quizData = quizData + quizObjectResults[i].id + " "
+			}
+			// add no extra space if last id in list
+			else {
+				quizData = quizData + quizObjectResults[i].id
+			}
 		}
-		//alert('Quiz Results (Object): ' + quizObjectResults);
-		//alert('Quiz Results (Array): ' + quizArrayResults);
-		//alert('Quiz Results (String): ' + quizStringResults);
-		quizData = JSON.stringify(quizArrayResults);
-
+		
 //	        connectWith: '.object',
   //              update: function() {
     //                $.ajax({
       //                    type: "POST",
         //                  data: quizData,
           //                url: "quiz/submit"
-            //            });
+            //            });		
+
 		alert('Data to POST: ' + quizData);
-		$.post('quiz/submit', function(quizData) { // data is the the data we want to send
+		$.post('quiz/submit', function(quizData) { // data is the the data we want to submit
 			//handle any other tasks once the server returns with status
 		});
 	});
