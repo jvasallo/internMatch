c2c2a08c3fa35fc4e74e373bc4213e7262b6c145
Joel Vasallo <joelvasallo@gmail.com>
2013-02-16 01:51:55 -0600

    included account as an INSTALLED_APP for routes for templates

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'internmatch',                      # Or path to database file if using sqlite3.
+        'NAME': 'internmatch',                      # Or path to database file if using sqlite3. internmatch
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -128,7 +128,8 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',    
     'register',
-    
+    'quiz',
+    'account',
     #'registration',
     'crispy_forms'
 )
