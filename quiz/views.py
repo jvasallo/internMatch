19810df291a983ab95d894aa8183e374a6b8da04
Joel Vasallo <joelvasallo@gmail.com>
2013-02-21 07:00:32 -0600

    Allows for multiple users to input data. Data is being posted to db

--- quiz/views.py
+++ quiz/views.py
@@ -2,7 +2,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
-from quiz.models import Quiz
+from quiz.models import Quiz, QuizResult
 
 def index(request):
     if request.user.is_authenticated():
@@ -29,7 +29,19 @@ def submit(request):
     if request.user.is_authenticated(): # if user is logged in https://docs.djangoproject.com/en/dev/topics/forms/?from=olddocs
         user = request.user
         if request.method == 'POST': # and if request is a POST
-         #    q = QuizResult.create('QuizResult', user.id)  # init a null quiz result
+            quiz = Quiz.objects.get(id=int(request.POST.get('quizID').encode('ascii','ignore')))
+            try:
+                user_quizresult = QuizResult.objects.get(user=user)
+            except Exception:
+                user_quizresult = None
+
+            if user_quizresult:
+                quizData = str(request.POST.get('quizString').encode('ascii','ignore'))
+                user_quizresult.collectQuizData(quizData)
+            else:
+                user_quizresult = QuizResult.create(quiz, user)  # init a null quiz result
+                quizData = str(request.POST.get('quizString').encode('ascii','ignore'))
+                user_quizresult.collectQuizData(quizData)
             return HttpResponseRedirect('quiz/complete/') # redirect to a thank you page or something
     else: # else user needs to log in
         return HttpResponseRedirect('/login') # redirect to some result page to show the "result"
