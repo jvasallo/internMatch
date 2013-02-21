5a27e022222adb3d2400e28d4e8a235e4d442e5d
Joel Vasallo <joelvasallo@gmail.com>
2013-02-21 04:55:31 -0600

    Updated quiz model to support base.html and also did some path changes. Preliminary work on posting to db

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,7 +3,7 @@ from django.views.generic import DetailView, ListView, TemplateView
 
 urlpatterns = patterns('',
     url(r'^$', 'quiz.views.index'),
-    url(r'begin/$', TemplateView.as_view(template_name="quiz/begin.html")),
+    url(r'quiz/$', 'quiz.views.quiz'),
     url(r'submit/$', 'quiz.views.submit'),
-    url(r'complete/$', TemplateView.as_view(template_name="quiz/complete.html")),
+    url(r'complete/$', 'quiz.views.complete'),
 )
--- quiz/views.py
+++ quiz/views.py
@@ -6,18 +6,30 @@ from quiz.models import Quiz
 
 def index(request):
     if request.user.is_authenticated():
-        quiz = Quiz.objects.get(id=1)
-        return render_to_response('quiz/quiz.html', {'quiz': quiz}, context_instance=RequestContext(request))
+        return render_to_response('quiz/index.html', {'user': request.user}, context_instance=RequestContext(request))
     else:
-        return HttpResponseRedirect('/login') # redirect to some result page to show the "result"
+        return HttpResponseRedirect('/login')
 
+def quiz(request):
+    if request.user.is_authenticated():
+        quiz = Quiz.objects.get(id=1)
+        user = request.user
+        return render_to_response('quiz/quiz.html', {'quiz': quiz, 'user': user}, context_instance=RequestContext(request))
+    else:
+        return HttpResponseRedirect('/login')
+
+def complete(request):
+    if request.user.is_authenticated():
+        return render_to_response('quiz/complete.html', {'user': request.user}, context_instance=RequestContext(request))
+    else:
+        return HttpResponseRedirect('/login')
 
 @csrf_exempt
 def submit(request):
     if request.user.is_authenticated(): # if user is logged in https://docs.djangoproject.com/en/dev/topics/forms/?from=olddocs
+        user = request.user
         if request.method == 'POST': # and if request is a POST
-         #   q = Quiz.create(request.user.user_id)  # init a null quiz result
-         #   q.collectQuizData(request.POST) # use the request to sent the data to the def collectQuizData
+         #    q = QuizResult.create('QuizResult', user.id)  # init a null quiz result
             return HttpResponseRedirect('quiz/complete/') # redirect to a thank you page or something
     else: # else user needs to log in
         return HttpResponseRedirect('/login') # redirect to some result page to show the "result"
