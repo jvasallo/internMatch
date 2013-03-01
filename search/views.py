a6a0e0c7839dcc9d8433318328a81ea9048f48ef
Joel Vasallo <joelvasallo@gmail.com>
2013-03-01 00:27:56 -0600

    Added dynamic search for company, intern, and anon. - Anon = See ALL Job Posts - Intern = See All Job Posts that have matching quizResult - Company = See All Interns that have matching quizResult

--- search/views.py
+++ search/views.py
@@ -7,17 +7,32 @@ from quiz.models import QuizResult
 from job_post.models import JobPost
 
 def index(request):
-    postings = JobPost.objects.all()
     if request.user.is_authenticated():
         user = request.user
         userProfile = request.user.get_profile()
         if userProfile.is_intern:
-            return render_to_response('search/job_search.html', {'intern': user, 'postings': postings, 'isAuth': request.user.is_authenticated()}, context_instance=RequestContext(request))
+            try:
+                postingList = JobPost.objects.all()
+                postings = []
+                for eachPosting in postingList:
+                    if eachPosting.company.quizResult() == userProfile.quizResult():
+                        postings.append(eachPosting)
+            except Exception:
+                postings = None 
+            return render_to_response('search/job_search.html', {'user': user, 'userProfile' : userProfile, 'postings': postings}, context_instance=RequestContext(request))
         else:
-            return render_to_response('search/intern_search.html', {'company': user}, context_instance=RequestContext(request))
+            try:
+                userList = User.objects.all()
+                userProfileList = []
+                interns = []
+                for eachUser in userList:
+                    userProfileList.append(eachUser.get_profile())
+                for eachProfile in userProfileList:
+                    if eachProfile.is_intern and eachProfile.quizResult() == userProfile.quizResult():
+                        interns.append(eachProfile)
+            except Exception:
+                interns = None
+            return render_to_response('search/intern_search.html', {'user': user, 'userProfile' : userProfile, 'interns' : interns}, context_instance=RequestContext(request))
     else:
-        return render_to_response('search/job_search.html', {'intern': None, 'postings': postings}, context_instance=RequestContext(request))
-
-def AllResults(request):
-    postings = JobPost.objects.all()
-    return render_to_response('search.html', {'postings': postings, 'isAuth': request.user.is_authenticated()}, context_instance=RequestContext(request))
+        postings = JobPost.objects.all()
+        return render_to_response('search/job_search.html', {'user': None, 'postings': postings}, context_instance=RequestContext(request))
