f6cd5d8e30a8964ef8601fef7aab7b9f984009ab
David <david.kingman@gmail.com>
2013-03-06 23:05:19 -0600

    Some search stuff

--- search/views.py
+++ search/views.py
@@ -7,48 +7,55 @@ from quiz.models import QuizResult
 from job_post.models import JobPost
 from datetime import date
 
+#TODO THIS METHOD IS WAAAAAY TO BIG
 def index(request):
-    if request.user.is_authenticated() and request.method == 'GET':
-        user = request.user
-        userProfile = request.user.get_profile()
-        if userProfile.is_intern:
-            try:
-                postings = JobPost.objects.filter(date_post_ends__gte=datetime.date.today())[:1]
-                postingList = JobPost.objects.all()
-                postings = []
-                for eachPosting in postingList:
-                    try:
-                        postingCompany = User.objects.get(id=eachPosting.company.id)
-                        if postingCompany.is_active and eachPosting.company.quizResult() == userProfile.quizResult():
-                            postings.append(eachPosting)
-                    except Exception:
-                        print 'skip'
-            except Exception:
-                postings = None
-            context = {'user': user, 'userProfile' : userProfile, 'postings': postings}
-            return render_to_response('search/job_search.html', context, context_instance=RequestContext(request))
+#    import pdb; pdb.set_trace()
+    if request.method == 'GET':
+        keyword = request.GET.get('keyword',False)
+        location = request.GET.get('location',False)
+        search_terms = {'keyword': keyword, 'location': location}
+        
+        if request.user.is_authenticated():
+            user = request.user
+            userProfile = request.user.get_profile()
+            if userProfile.is_intern:
+                try:
+                    postings = JobPost.objects.filter(date_post_ends__gte=datetime.date.today())[:1]
+                    postingList = JobPost.objects.all()
+                    postings = []
+                    for eachPosting in postingList:
+                        try:
+                            postingCompany = User.objects.get(id=eachPosting.company.id)
+                            if postingCompany.is_active and eachPosting.company.quizResult() == userProfile.quizResult():
+                                postings.append(eachPosting)
+                        except Exception:
+                            print 'skip'
+                except Exception:
+                    postings = None
+                context = {'user': user, 'userProfile' : userProfile, 'postings': postings, 'search_terms': search_terms}
+                return render_to_response('search/job_search.html', context, context_instance=RequestContext(request))
+            else:
+                try:
+                    userList = User.objects.all()
+                    userProfileList = []
+                    interns = []
+                    for eachUser in userList:
+                        if eachUser.is_active:
+                            userProfileList.append(eachUser.get_profile())
+                    for eachProfile in userProfileList:
+                        if eachProfile.is_intern and eachProfile.quizResult() == userProfile.quizResult():
+                            interns.append(eachProfile)
+                except Exception:
+                    interns = None
+                return render_to_response('search/intern_search.html', {'user': user, 'userProfile' : userProfile, 'interns' : interns, 'search_terms':search_terms}, context_instance=RequestContext(request))
         else:
-            try:
-                userList = User.objects.all()
-                userProfileList = []
-                interns = []
-                for eachUser in userList:
-                    if eachUser.is_active:
-                        userProfileList.append(eachUser.get_profile())
-                for eachProfile in userProfileList:
-                    if eachProfile.is_intern and eachProfile.quizResult() == userProfile.quizResult():
-                        interns.append(eachProfile)
-            except Exception:
-                interns = None
-            return render_to_response('search/intern_search.html', {'user': user, 'userProfile' : userProfile, 'interns' : interns}, context_instance=RequestContext(request))
-    else:
-        postingList = JobPost.objects.all()
-        postings = []
-        for eachPosting in postingList:
-            try:
-                postingCompany = User.objects.get(id=eachPosting.company.id)
-                if postingCompany.is_active:
-                    postings.append(eachPosting)        
-            except Exception:
-                print 'skip'
-        return render_to_response('search/job_search.html', {'user': None, 'postings': postings}, context_instance=RequestContext(request))
+            postingList = JobPost.objects.all()
+            postings = []
+            for eachPosting in postingList:
+                try:
+                    postingCompany = User.objects.get(id=eachPosting.company.id)
+                    if postingCompany.is_active:
+                        postings.append(eachPosting)        
+                except Exception:
+                    print 'skip'
+            return render_to_response('search/job_search.html', {'user': None, 'postings': postings, 'search_terms':search_terms}, context_instance=RequestContext(request))
