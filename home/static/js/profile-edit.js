ab11d3041b22834afe37ffb8fd188cf8b32763ee
Juan Carlos Escobar <escobar.jc7@gmail.com>
2013-03-12 22:17:04 -0500

    fixed issue that allowed company to edit their state to an empty value: removed predictive search from state field

--- home/static/js/profile-edit.js
+++ home/static/js/profile-edit.js
@@ -107,15 +107,63 @@ $(function(){
 		inputclass: 'input-medium'
     });
 	
-	var states = []; // states list
-    $.each({"AL": "Alabama", "AK": "Alaska", "AZ": "Arizona", "AR": "Arkansas", "CA": "California", "CO": "Colorado", "CT": "Connecticut", "DE": "Delaware", "DC": "Dist of Columbia", "FL": "Florida", "GA": "Georgia", "HI": "Hawaii", "ID": "Idaho", "IL": "Illinois", "IN": "Indiana", "IA": "Iowa", "KS": "Kansas", "KY": "Kentucky", "LA": "Louisiana", "ME": "Maine", "MD": "Maryland", "MA": "Massachusetts", "MI": "Michigan", "MN": "Minnesota", "MS": "Mississippi", "MO": "Missouri", "MT": "Montana", "NE": "Nebraska", "NV": "Nevada", "NH": "New Hampshire", "NJ": "New Jersey", "NM": "New Mexico", "NY": "New York", "NC": "North Carolina", "ND": "North Dakota", "OH": "Ohio", "OK": "Oklahoma", "OR": "Oregon", "PA": "Pennsylvania", "RI": "Rhode Island", "SC": "South Carolina", "SD": "South Dakota", "TN": "Tennessee", "TX": "Texas", "UT": "Utah", "VT": "Vermont", "VA": "Virginia", "WA": "Washington", "WV": "West Virginia", "WI": "Wisconsin", "WY": "Wyoming"}, function(k, v) {
-        states.push({id: k, text: v});
-    });
-    $('#state').editable({ // state field (required)
-		type: 'select2', // select2.js
+	$('#state').editable({ // state field (required)
+		type: 'select',
 		inputclass: 'input-large',
-		source: states
-    });
+		source: [
+			{value: 'AL', text: 'Alabama'},
+			{value: 'AK', text: 'Alaska'},
+			{value: 'AZ', text: 'Arizona'},
+			{value: 'AR', text: 'Arkansas'},
+			{value: 'CA', text: 'California'},
+			{value: 'CO', text: 'Colorado'},
+			{value: 'CT', text: 'Connecticut'},
+			{value: 'DE', text: 'Delaware'},
+			{value: 'DC', text: 'Dist of Columbia'},
+			{value: 'FL', text: 'Florida'},
+			{value: 'GA', text: 'Georgia'},
+			{value: 'HI', text: 'Hawaii'},
+			{value: 'ID', text: 'Idaho'},
+			{value: 'IL', text: 'Illinois'},
+			{value: 'IN', text: 'Indiana'},
+			{value: 'IA', text: 'Iowa'},
+			{value: 'KS', text: 'Kansas'},
+			{value: 'KY', text: 'Kentucky'},
+			{value: 'LA', text: 'Louisiana'},
+			{value: 'ME', text: 'Maine'},
+			{value: 'MD', text: 'Maryland'},
+			{value: 'MA', text: 'Massachusetts'},
+			{value: 'MI', text: 'Michigan'},
+			{value: 'MN', text: 'Minnesota'},
+			{value: 'MS', text: 'Mississippi'},
+			{value: 'MO', text: 'Missouri'},
+			{value: 'MT', text: 'Montana'},
+			{value: 'NE', text: 'Nebraska'},
+			{value: 'NV', text: 'Nevada'},
+			{value: 'NH', text: 'New Hampshire'},
+			{value: 'NJ', text: 'New Jersey'},
+			{value: 'NM', text: 'New Mexico'},
+			{value: 'NY', text: 'New York'},
+			{value: 'NC', text: 'North Carolina'},
+			{value: 'ND', text: 'North Dakota'},
+			{value: 'OH', text: 'Ohio'},
+			{value: 'OK', text: 'Oklahoma'},
+			{value: 'OR', text: 'Oregon'},
+			{value: 'PA', text: 'Pennsylvania'},
+			{value: 'RI', text: 'Rhode Island'},
+			{value: 'SC', text: 'South Carolina'},
+			{value: 'SD', text: 'South Dakota'},
+			{value: 'TN', text: 'Tennessee'},
+			{value: 'TX', text: 'Texas'},
+			{value: 'UT', text: 'Utah'},
+			{value: 'VT', text: 'Vermont'},
+			{value: 'VA', text: 'Virginia'},
+			{value: 'WA', text: 'Washington'},
+			{value: 'WV', text: 'West Virginia'},
+			{value: 'WI', text: 'Wisconsin'},
+			{value: 'WY', text: 'Wyoming'}
+		]
+	});
 	
     $('#zip').editable({ // zip field
 		validate: function(value) { // required
