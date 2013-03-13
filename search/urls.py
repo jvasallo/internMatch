693dfbdb42b597d962ee309726d9b4cf2e69f881
David <david.kingman@gmail.com>
2013-03-13 01:39:31 -0500

    Basic keyword search working Urls need to be fixed still

--- search/urls.py
+++ search/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-    url(r'^$', 'search.views.index'),
+    url(r'^$', 'search.views.router'),
+    url(r'^intern$', 'search.views.intern'),
 )
--- search/views.py
+++ search/views.py
@@ -1,32 +1,87 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import get_object_or_404, render_to_response
+from django.shortcuts import get_object_or_404, render_to_response, redirect
 from django.contrib.auth.models import User
 from django.template import RequestContext
 from quiz.models import QuizResult
 from job_post.models import JobPost
 from datetime import date
 
-#TODO THIS METHOD IS WAAAAAY TO BIG
+
+# Filter the search request by intern or company, passing along any GET parameters
+def router(request):
+#    import pdb; pdb.set_trace()
+    response = redirect('/search/intern')
+    if request.user.is_authenticated():
+        user_profile = request.user.get_profile()
+        if not user_profile.is_intern:
+            response = redirect('search/company')
+    get_params = '?keyword=' + request.GET.get('keyword','') + '&location=' + request.GET.get('location','') + '&page=' + request.GET.get('page','1')
+    response['Location'] += get_params
+    return response
+    
+    
+# intern or anonomous user search results
+def intern(request):
+    if request.user.is_authenticated():
+        user_profile = request.user.get_profile()
+        if not user_profile.is_intern:
+            return redirect('search/company')
+    keyword = request.GET.get('keyword',False)
+    location = request.GET.get('location',False)
+    search_terms = {'keyword': keyword, 'location': location}
+    page = request.GET.get('page',False)
+    postings = []
+#    import pdb; pdb.set_trace()
+    posting_list = getPostingsFromKeyword(keyword)
+    posting_list = getPostingsByPage(posting_list,page)
+#    JobPost.objects.all().order_by('date_post_ends')[k:n]
+    for posting in posting_list:
+        posting_company = User.objects.get(id=posting.company.id)
+        if posting_company.is_active:
+            postings.append(posting)
+    if request.user.is_authenticated(): 
+        context = {'user': request.user, 'userProfile' : user_profile, 'postings': postings, 'search_terms': search_terms}
+    else:
+        context = {'user': None, 'postings': postings, 'search_terms':search_terms}
+    return render_to_response('search/job_search.html', context, context_instance=RequestContext(request))
+    
+    
+# Get postings by page(assume ordered by date), 5 postings per page
+def getPostingsByPage(postings, page):
+    per_page = 5
+    page = int(page) - 1
+    if page:
+        slice = per_page*page
+        return postings[slice:slice+per_page]
+    return postings[:per_page]
+
+def getPostingsFromKeyword(keyword):
+    if keyword:
+        return JobPost.objects.filter(skill__name__contains=keyword).distinct().filter(date_post_ends__gte=date.today()).order_by('date_post_ends')
+    return JobPost.objects.filter(date_post_ends__gte=date.today()).distinct().order_by('date_post_ends')
+
+
 def index(request):
 #    import pdb; pdb.set_trace()
     if request.method == 'GET':
         keyword = request.GET.get('keyword',False)
         location = request.GET.get('location',False)
         search_terms = {'keyword': keyword, 'location': location}
+#        import pdb; pdb.set_trace()
         
         if request.user.is_authenticated():
             user = request.user
             userProfile = request.user.get_profile()
             if userProfile.is_intern:
                 try:
-                    postings = JobPost.objects.filter(date_post_ends__gte=datetime.date.today())[:1]
-                    postingList = JobPost.objects.all()
+                    posting_list = getPostingsFromKeyword(keyword)
+                    posting_list = getPostingsByPage(page)
                     postings = []
-                    for eachPosting in postingList:
+                    for eachPosting in posting_list:
                         try:
                             postingCompany = User.objects.get(id=eachPosting.company.id)
-                            if postingCompany.is_active and eachPosting.company.quizResult() == userProfile.quizResult():
+                            if postingCompany.is_active:# and eachPosting.company.quizResult() == userProfile.quizResult():
                                 postings.append(eachPosting)
                         except Exception:
                             print 'skip'
@@ -49,7 +104,7 @@ def index(request):
                     interns = None
                 return render_to_response('search/intern_search.html', {'user': user, 'userProfile' : userProfile, 'interns' : interns, 'search_terms':search_terms}, context_instance=RequestContext(request))
         else:
-            postingList = JobPost.objects.all()
+            postingList = JobPost.objects.filter(skill__name__contains=keyword).distinct().filter(date_post_ends__gte=date.today())
             postings = []
             for eachPosting in postingList:
                 try:
@@ -59,3 +114,4 @@ def index(request):
                 except Exception:
                     print 'skip'
             return render_to_response('search/job_search.html', {'user': None, 'postings': postings, 'search_terms':search_terms}, context_instance=RequestContext(request))
+        
